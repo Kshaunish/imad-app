21b0774d3963f0c42111417e57e2f4f878e19e7a
Kshaunish <30623639+Kshaunish@users.noreply.github.com>
2017-08-12 22:55:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,6 @@ button.onclick = function () {
     //Create a request object
     var request = new XMLHttpRequest();
     
-    
     //Capture and store the response in a variable
     request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
@@ -15,7 +14,6 @@ button.onclick = function () {
               var counter = request.responseText;
               var span = document.getElementById('count');
               span.innerHTML = counter.toString();
-
           }
       }
       // Not done yet
@@ -24,4 +22,20 @@ button.onclick = function () {
     //Make the request
     request.open('GET','http://vedace2011.imad.hasura-app.io/counter', true);
     request.send(null);
+};
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  //Should make arequest to the server and send the name
+  //Capture a list of names and render it as a list.
+  var names = ['name1','name2','name3','name4'];
+  var list = '';
+  for (var i=0;i<name.length ;i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
 };
