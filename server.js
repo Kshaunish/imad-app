aa8164956887082a2007020cb6f51b67b2b1096e
Kshaunish <30623639+Kshaunish@users.noreply.github.com>
2017-09-10 00:00:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,20 @@ app.post('/create-user', function(req, res){
    });
 });
 
+app.post('/login', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    
+   
+    pool.query('SELECT * FROM WHERE "user" username = $1', [username], function (err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           
+           res.send('User successfully created : ' +  username);
+       }
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res){
    // make a select request
